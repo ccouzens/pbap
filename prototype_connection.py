c7d7bab7ab7fa8f3b87159804718be12e0ff0109
Chris Couzens <ccouzens@gmail.com>
2013-04-05 23:15:59 +0100

    Get the pbap server port name dynamically

--- prototype_connection.py
+++ prototype_connection.py
@@ -1,4 +1,5 @@
-from bluetooth import discover_devices, lookup_name, BluetoothSocket, RFCOMM
+#!/usr/bin/env python
+from bluetooth import discover_devices, lookup_name, BluetoothSocket, RFCOMM, find_service
 from sys import argv
 
 if __name__ == '__main__':
@@ -13,7 +14,14 @@ if __name__ == '__main__':
   if target_address is None:
     raise Exception("Could not find bluetooth device")
   print "found", target_address
+  pbap_servers = find_service(uuid='112F', address=target_address)
+  number_of_matching_servers = len(pbap_servers)
+  if number_of_matching_servers != 1:
+    print "Found %d matching servers" % number_of_matching_servers
+  if number_of_matching_servers == 0:
+    raise Exception("No PBAP servers found")
+  pbap_server = pbap_servers[0]
   sock = BluetoothSocket( RFCOMM )
-  sock.connect((target_address, 19))
+  sock.connect((target_address, pbap_server['port']))
   print "Socket established"
   sock.close()
