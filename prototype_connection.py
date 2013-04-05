228f4ad1a958759511130fb9f417dc0081e3e584
Chris Couzens <ccouzens@gmail.com>
2013-04-05 23:28:44 +0100

    Look for pbap services then the correct device, rather than device then service

--- prototype_connection.py
+++ prototype_connection.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
-from bluetooth import discover_devices, lookup_name, BluetoothSocket, RFCOMM, find_service
+from bluetooth import lookup_name, BluetoothSocket, RFCOMM, find_service
 from sys import argv
 
 if __name__ == '__main__':
   target_name = argv[1]
   print "looking for", target_name
   target_address = None
-  nearby_devices = discover_devices()
-  for address in nearby_devices:
+  pbap_servers = find_service(uuid='112F')
+  pbap_devices = set(pbap_server['host'] for pbap_server in pbap_servers)
+
+  print "found %d PBAP server devices hosting %d PBAP servers" % (len(pbap_devices), len(pbap_servers))
+
+  for address in pbap_devices:
     if target_name == lookup_name(address):
       target_address = address
       break
   if target_address is None:
     raise Exception("Could not find bluetooth device")
   print "found", target_address
-  pbap_servers = find_service(uuid='112F', address=target_address)
+  pbap_servers = [pbap_server for pbap_server in pbap_servers if pbap_server['host'] == target_address]
   number_of_matching_servers = len(pbap_servers)
   if number_of_matching_servers != 1:
     print "Found %d matching servers" % number_of_matching_servers
