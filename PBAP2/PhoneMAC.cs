a5cb6c71cb210e8fd022327164c7faaf539a9ec2
Chris Couzens <ccouzens@gmail.com>
2013-05-27 22:19:26 +0100

    Move my phone bluetooth address to a separate file so I don't accidentally include it

---
+++ PBAP2/PhoneMAC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBAP2
+{
+    class PhoneMAC
+    {
+        public const string macAddress = "00:00:00:00:00:00";
+    }
+}
