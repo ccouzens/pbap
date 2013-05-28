c2f830f91c5196f5c2cbd847e5c246c7096c672a
Chris Couzens <ccouzens@gmail.com>
2013-05-28 21:51:49 +0100

    Further develop c# pbap
    It now raises an error. Unexpected OBEX response code: 0xC6 (NotAcceptable).
    
    The pbap guide says:
    The request is recognized and all the parameter values are legal, but there is a problem with a parameter value that indicates a request that cannot be met by the Server.

--- PBAP2/Program.cs
+++ PBAP2/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Brecham.Obex;
 
 namespace PBAP2
 {
@@ -16,6 +17,11 @@ namespace PBAP2
             BluetoothAddress addr = BluetoothAddress.Parse(PhoneMAC.macAddress);
             BluetoothClient c = new BluetoothClient();
             c.Connect(addr, BluetoothService.PhonebookAccessPse);
+            using (ObexClientSession obexClient = new ObexClientSession(c.GetStream(), UInt16.MaxValue))
+            {
+                Guid targetHeader = new Guid("796135f0-f0c5-11d8-0966-0800200c9a66");
+                obexClient.Connect(targetHeader.ToByteArray());
+            }
         }
     }
 }
